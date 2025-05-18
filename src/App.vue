263913989be0caa89629379c0d31fe4f6a2a16f4
sajalpaudyal <paudyalsajal@gmail.com>
2025-05-18 14:15:36 +0200

    created a basic to-do application using vue js to understand the basics

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import type { Todo } from './types/Todo'
+
+const todos: Ref<Todo[]> = ref([
+  { id: 1, text: 'Learn Vue Js', completed: false },
+  { id: 2, text: 'Build a to-do application', completed: false },
+  { id: 3, text: 'Push it onto github', completed: false },
+])
+
+const newTodoText: Ref<String> = ref('')
+
+function addTodo(): void {
+  if (newTodoText.value.trim()) {
+    todos.value.push({
+      id: Date.now(),
+      text: newTodoText.value.trim(),
+      completed: false,
+    })
+
+    newTodoText.value = ''
+  }
+}
+
+function toggleTodo(todo: Todo): void {
+  todo.completed = !todo.completed
+}
+
+function removeTodo(todoId: number) {
+  todos.value = todos.value.filter((todo) => todo.id != todoId)
+}
+</script>
+
+<template>
+  <main>
+    <h1>To-Do list using Vue 3</h1>
+
+    <div class="input-section">
+      <input type="text" v-model="newTodoText" placeholder="Add a todo" @keypress.enter="addTodo" />
+      <button @click="addTodo">Add todo</button>
+    </div>
+
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        @click="toggleTodo(todo)"
+        :class="{ completed: todo.completed }"
+      >
+        {{ todo.text }}
+        <button @click="removeTodo(todo.id)" class="remove-button">X</button>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 500px;
+  margin: 40px auto;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 8px;
+  font-family: sans-serif;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+input[type='text'] {
+  padding: 8px;
+  margin-right: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 8px 12px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+
+.input-section {
+  margin-bottom: 20px;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: #888;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+  padding: 8px;
+  border-bottom: 1px solid #eee;
+}
+
+li span {
+  flex-grow: 1;
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+.remove-button {
+  background-color: #f44336;
+  padding: 4px 8px;
+  font-size: 0.8em;
+}
+
+.remove-button:hover {
+  background-color: #d32f2f;
+}
+</style>
